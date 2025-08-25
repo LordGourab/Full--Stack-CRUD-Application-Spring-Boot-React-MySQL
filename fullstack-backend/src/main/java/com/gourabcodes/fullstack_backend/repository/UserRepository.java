aa4d1f59eb04eded342827675cacde7d1e8459db
Gourab Kunda Roy <150722367+LordGourab@users.noreply.github.com>
2025-08-25 23:54:30 +0530

    Delete fullstack-backend directory

--- fullstack-backend/src/main/java/com/gourabcodes/fullstack_backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gourabcodes.fullstack_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gourabcodes.fullstack_backend.model.User;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
