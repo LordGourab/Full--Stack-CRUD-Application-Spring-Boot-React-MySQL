05e72f49352022cbcaefa04577b9221359f15893
Gourab Kunda Roy <150722367+LordGourab@users.noreply.github.com>
2025-08-25 23:36:58 +0530

    Add files via upload

---
+++ fullstack-backend/src/main/java/com/gourabcodes/fullstack_backend/exception/UserNotFoundException.java
@@ -0,0 +1,10 @@
+package com.gourabcodes.fullstack_backend.exception;
+
+public class UserNotFoundException extends RuntimeException{
+	
+	public UserNotFoundException(Long id)
+	{
+		super("Could not find the user with id: "+id);
+	}
+
+}
